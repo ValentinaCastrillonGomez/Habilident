34065771e747f4d1c152952072a6aabe766115b7
juan arboleda <jarboledatolosa@hotmail.com>
2024-11-06 11:27:04 -0500

    cambios en estilos

--- Frontend/src/app/core/components/navbar/navbar.component.ts
+++ Frontend/src/app/core/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 import { LoadingService } from '@core/services/loading.service';
 import { NotificationService } from '@core/services/notification.service';
+import { RecordComponent } from '@features/records/components/record/record.component';
 import { MaterialModule } from '@shared/modules/material/material.module';
+import { FormatsService } from '@shared/services/formats.service';
 import { paths } from 'src/app/app.routes';
 
 @Component({
@@ -13,12 +16,15 @@ import { paths } from 'src/app/app.routes';
     RouterLink,
     MaterialModule
   ],
+  providers: [FormatsService],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   private readonly authService = inject(AuthService);
   private readonly notificationService = inject(NotificationService);
+  private readonly formatsService = inject(FormatsService);
+  private readonly dialog = inject(MatDialog);
   loadingService = inject(LoadingService);
 
   readonly administration = [
@@ -34,9 +40,16 @@ export class NavbarComponent {
 
   username = this.authService.getUser().name;
   notifications = this.notificationService.notifications;
+  showAlerts = false;
 
   logout() {
     this.authService.logout();
+  }
+
+  async open(formatId: string) {
+    this.showAlerts = false;
+    const format = await this.formatsService.get(formatId);
+    this.dialog.open(RecordComponent, { data: { format } });
   }
 
 }
